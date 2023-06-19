b3548dd0701b9af04410d8cd01a67bc59c6014ef
KG <karlgustav.ek@hotmail.com>
2023-06-19 13:34:38 +0200

    added channel functionality to block non authorized users from non public channels

--- src/components/Channels.jsx
+++ src/components/Channels.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react'
 import { addChannel, deleteChannel, getChannels } from '../channelEndpoints'
 import Messages from './Messages'
+import { loginState } from './Data/atoms'
+import { useRecoilValue } from 'recoil'
 
 
 
@@ -10,6 +12,7 @@ function Channels() {
     const [selectedChannel, setSelectedChannel] = useState(null);
     // const [messages, setMessages] = useState([]); // Ny state-variabel för meddelanden
     const [channelMessages, setChannelMessages] = useState([]);
+    const isLoggedIn = useRecoilValue(loginState)
 
     const channelName = channel.find(c => c.id == selectedChannel)?.name
 
@@ -69,6 +72,8 @@ function Channels() {
         }
     };
 
+    console.log(isLoggedIn);
+    console.log(channel);
     return (
         <div>
             <hr />
@@ -90,12 +95,26 @@ function Channels() {
                     <div>
                         <h3>[Kanaler]:</h3>
                     </div>
-                    {channel.map((channel) => (
-                        <h3 className='channel' key={channel.id} onClick={() => handleChannelClick(channel.id)}>
-                            <p onClick={() => setSelectedChannel(channel.id)}> #{channel.name}</p>
-                            {/* <button onClick={() => handleDeleteChannel(channel.id)}>Delete Channel</button> */}
-                        </h3>
-                    ))}
+                    {channel
+                        .filter((channel) => channel.public == true || isLoggedIn)
+                        .map((channel) => (
+                            <h3
+                                className="channel"
+                                key={channel.id}
+                                onClick={() => handleChannelClick(channel.id)}
+                            >
+                                <p onClick={() => setSelectedChannel(channel.id)}>#{channel.name}</p>
+                            </h3>
+                        ))}
+                    {/* Render locked channels */}
+                    {channel
+                        .filter((channel) => channel.public == false && !isLoggedIn)
+                        .map((channel) => (
+                            <h3 className="channel" key={channel.id}>
+                                {channel.name} 🔒
+                            </h3>
+                        ))}
+                    {/* <button onClick={() =>  handleDeleteChannel(channel.id)}>Delete Channel</button> */}
                 </section>
                 <br />
             </div>
